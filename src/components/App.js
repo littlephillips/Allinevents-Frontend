f366d495c610e6cf18f9cfc593587e33ec50b39e
littlephillips <phillipkinuthia72@gmail.com>
2022-12-14 12:41:49 +0300

    folder structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Footer from './Footer'
+import LoginForm from './LoginForm'
+import Navbar from './Navbar'
+
+function App() {
+  return(
+    <>
+    <h1>Hello</h1>
+    <Footer />
+    <LoginForm />
+    <Navbar />
+    </>
+  )
+}
+
+export default App;
