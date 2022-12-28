35e3fc180d0fa7d963a1c08a430e90944f42fbb0
first-name second-name <silvia.wachira@student.moringa.com>
2022-12-28 12:08:32 +0300

    feature: create new event form

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Admin() {
     return (
-    <div>Admin</div>
+        <div className='w-full h-screen'>
+            <div className='w-full h-full px-2 px-8'>
+                <button className='py-1 px-2 border border-yellow-400 rounded-full'>create an event</button>
+            </div>
+        </div>
     )
 }
 
-export default Admin 
+export default Admin    
