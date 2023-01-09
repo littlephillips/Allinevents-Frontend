af014cf9708a9b36b142a7b89cbc11e35d705dc1
+ <+>
2023-01-09 04:23:04 -0500

    LLNVNT-25 login page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,16 +3,7 @@ import '../../src/dist/output.css'
 
 function HomePage() {
   return (
-    <section className='flex bg-teal-600'>
-      <h1>Your one Stop for all you events and audiences</h1>
-      <p>What kind of event are you planning?</p>
-      <div>
-        <a href='#'>In-Person</a>
-        <a href='#'>Hybrid</a>
-        <a href='#'>Virtual</a>
-        <a href='#'>Webinar</a>
-      </div>
-    </section>
+   <h1>HomePage</h1>
   )
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,46 @@ import React from 'react'
 
 function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <div className='flex min-w-full bg-slate-400 flex-col min-h-screen items-center justify-center'>   
+
+      <form className='flex flex-col p-10 text-white bg-slate-700 rounded-md shadow-sm'>
+        <h2 className='text-center text-2xl mb-5 m-auto '>Login</h2>
+        
+        <div>
+          <label>Email</label>
+          <br />
+            <input 
+              placeholder='email'
+              className='focus:outline-blue-600 text-black' 
+              type="email" 
+              name='email' 
+              required=''
+            
+            />
+        </div>
+
+        <div className='py-4'>
+          <label>Password</label>
+            <br />
+          <input
+            className='text-black'
+            type="password" 
+            name='password' 
+            required=''
+            
+          />
+        </div>
+        <br />
+
+        <button className='w-16 p-2 rounded-md bg-slate-500'>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit
+        </button>
+
+      </form>
+    </div>
   )
 }
 
