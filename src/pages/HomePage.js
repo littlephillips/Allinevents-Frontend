0f1e6042d69a2e7dfa5b27bdca4c5271faf93088
+ <+>
2023-01-09 01:54:18 -0500

    LLNVNT-11 configure tailwindcss

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../../src/dist/output.css'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <section className='flex bg-teal-600'>
+      <h1>Your one Stop for all you events and audiences</h1>
+      <p>What kind of event are you planning?</p>
+      <div>
+        <a href='#'>In-Person</a>
+        <a href='#'>Hybrid</a>
+        <a href='#'>Virtual</a>
+        <a href='#'>Webinar</a>
+      </div>
+    </section>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js, html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
