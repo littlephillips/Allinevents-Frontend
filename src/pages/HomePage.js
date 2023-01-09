3e19d1ca9b7cd5df7025d50ba7a31d3c72ae1054
+ <+>
2023-01-09 07:57:17 -0500

    LLNVNT-11 a little styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,13 @@ import '../../src/dist/output.css'
 
 function HomePage() {
   return (
-  <div className='p-24 w-full min-h-screen'>
-    <section className='mb-5 space-y-5'>
-      <h2 className='text-3xl'>Your one Stop for all Your events and audiences</h2>
-      <p className='pt-6'>What kind of event are you planning?</p>
-      <div className='pt-7 space-x-3'>
+    <main className="bg-[url('https://sandemanevents.com/wp-content/uploads/2019/01/AR-0423-e1548685326106-2000x1000.jpg')]">
+      <div className='text-center w-full min-h-screen flex flex-col items-center justify-center'>
+    <section
+      className=" mb-5  space-y-5">
+      <h2 className='text-7xl font-bold text-white outline-4 '>Your one Stop for all Your events and audiences</h2>
+      <p className='pt-6 text-5xl font-semibold text-[#233D4D]'>What kind of event are you planning?</p>
+      <div className='pt-7 space-x-9'>
         <a href='#' className='p-2 rounded-md bg-blue-300'>In-Person</a>
         <a href='#' className='p-2 rounded-md bg-blue-300'>Hybrid</a>
         <a href='#' className='p-2 rounded-md bg-blue-300'>Virtual</a>
@@ -15,7 +17,8 @@ function HomePage() {
       </div>
     </section>
 
-   <section className='flex flex-col'>
+   {/* <section 
+    className='flex flex-col'>
     <h1 className='text-center'>Our Services</h1>
     <p className='text-center'>Check out all our events</p>
     <div className='flex flex-row'>
@@ -28,9 +31,12 @@ function HomePage() {
         <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </card>
     </div>
-   </section>
+   </section> */}
 
     </div>
+
+    </main>
+  
    
   )
 }
