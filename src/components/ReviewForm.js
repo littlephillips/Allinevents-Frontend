19419971a34d87760cbe77c039789d77664afc33
Steve <stephenkago10@gmail.com>
2023-01-09 10:40:36 +0300

    Create review form

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'
+import './Reviews.css'
+
+function ReviewForm({handleNewReview}) {
+
+  const [formData,setFormData]=useState([{
+    name:"",
+    email: "",
+    body:"",
+  }])
+
+  const handleSaveReview = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+            const res = await fetch("https://my-json-server.typicode.com/StephenKairu/dummydbserver/Reviews", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: formData.name,
+                    email: formData.email,
+                    body: formData.body,
+                }),
+            });
+            const response = await res.json();
+            handleNewReview(response);
+            setFormData([{
+              name:"",
+              email: "",
+              body:"",
+            }]);
+
+    };
+
+
+  function handleChange(e){
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+  
+
+  return (
+    <div className="reviews">
+    <div id="reviews-container">
+        <div className="review-card">
+        </div>
+        <div className="form">
+          <br /> <br />
+            <h3 className="text-sky-500 text-2xl">Would you like to share your experience with us?</h3>
+            <br />
+            <form onSubmit={handleSaveReview} className="comment-form">
+            Name: <br/><input value={formData.name} onChange={handleChange} type="text" name="name" placeholder="Your name?" className="client-name" required /> <br/> <br/>
+            Email: <br/><input value={formData.email} onChange={handleChange} type="email" name="email" placeholder="Your email won't be published" className="email-input" required /> <br/><br/>
+            Comment: <br/>
+            <textarea value={formData.body} onChange={handleChange} name="body" className="client-text" cols="30" rows="6" placeholder="Leave a review..." required></textarea><br/><br/>
+            <button type="submit" className="h-10 px-6 font-semibold rounded-md text-white bg-sky-500 hover:bg-sky-700">Submit Review</button>
+            </form> <br/><br/>
+        </div>
+    </div>    
+</div>
+);
+}
+
+export default ReviewForm
