38bbf9f554b98ffb6313957e13ace9c0260910a5
isir10 <isir.malak@moringaschool.com>
2023-01-10 11:25:17 +0300

    LLNVNT-36 <added styling to my slider component>

--- src/App.js
+++ src/App.js
@@ -39,14 +39,14 @@ function App() {
         <>
             <Hero />
             <Slider
-                imageSrc={"https://media.istockphoto.com/id/479977238/photo/table-setting-for-an-event-party-or-wedding-reception.jpg?s=612x612&w=0&k=20&c=yIKLzW7wMydqmuItTTtUGS5cYTmrRGy0rXk81AltdTA="}
+                imageSrc={"https://images.squarespace-cdn.com/content/v1/57cf5e97414fb5a80f7a837b/1519410893820-Z6QV8X1Q4NFKBP6E9176/shutterstock_512157103.jpg?format=2500w"}
                 title={"One Step Event Planner."}
                 subtitle={
                   "At AllEvents, we promise to make every special event in your life unforgetable...…"
                 }
               />
               <Slider
-                imageSrc={"https://media.istockphoto.com/id/1177485677/photo/table-setting-for-an-event-party-or-wedding-reception.jpg?s=612x612&w=0&k=20&c=F1kd8_DGpxBmq02GLMywXHJbAuyg-50xW1xdsQNxDm0="}
+                imageSrc={"https://media.istockphoto.com/id/1368180747/photo/cake-cutting-during-wedding-ceremony-with-fireworks-in-the-background.jpg?s=612x612&w=0&k=20&c=LT6lIUPLTTP9FVGdI266Cq9cl1a3tCC7oPB0efKCiBw="}
                 title={"Celebrate Affordably."}
                 subtitle={"You dont have to break the bank to get your dream wedding...."}
                 flipped={true}
