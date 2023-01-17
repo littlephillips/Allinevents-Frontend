908eda3b3dd12db5f46ef8051c0e4b9ee1cabc5b
flipblip <wickynjy@gmail.com>
2023-01-17 20:50:36 +0300

    remove original code

--- src/pages/ServicePage.js
+++ src/pages/ServicePage.js
@@ -1,39 +1,5 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-// css
-// import "../css/servicepage.css"
-
-
-// function ServicePage({services}) {
-//   return (
-//     <div className="services-page">
-//       <h1 id="header">Our services</h1>
-//       <div id="content-box">
-//         {services.map((services) => (
-//           <div id="carde">
-//             <img src= {services.image_url} alt= 'service'/>
-//               <h2>{services.eventname}</h2>
-//               <p>
-//                   {services.description}
-//               </p>
-//               <ul>
-//                 <li>{services.services}</li>
-//               </ul>
-//               <div className='services-buttons'>
-//               <button className="service_button-book">
-//                 <NavLink to="/booking">Book</NavLink>
-//               </button>
-//               <button className="service_button-review">
-//                 <NavLink to="/reviews">Review</NavLink>
-//               </button>
-//           </div>
-            
-//           </div>
-//         ))}
-//       </div>
-//     </div>  
-//   )
-// }
 
 const ServicePage = ({services}) =>{
 
